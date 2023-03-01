e9b923bf30947a3365b7e4900e4b6c890585a534
Ndimofor <ndimaret@gmail.com>
2023-03-01 13:52:17 +0100

    add Book create form

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,17 @@
 import { useState } from "react";
 import "./App.css";
+import BookCreate from "./components/BookCreate";
 
 function App() {
   const [books, setBooks] = useState([]);
+
+  const createBook = (title) => {
+    console.log("You have submitted the following:", title);
+  };
   return (
-    <div className="App">
+    <div>
       <h1>Helloo</h1>
+      <BookCreate onCreate={createBook} />
     </div>
   );
 }
--- src/components/BookCreate.jsx
+++ src/components/BookCreate.jsx
@@ -1,7 +1,26 @@
-function BookCreate() {
+import { useState } from "react";
+
+function BookCreate({ onCreate }) {
+  const [title, setTitle] = useState("");
+
+  const handleChange = (event) => {
+    setTitle(event.target.value);
+  };
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    onCreate(title);
+    setTitle("");
+  };
+
   return (
     <div>
       <h1>BookCreate</h1>
+      <form onSubmit={handleSubmit}>
+        <label>Title</label>
+        <input value={title} onChange={handleChange} />
+        <button>Create</button>
+      </form>
     </div>
   );
 }
