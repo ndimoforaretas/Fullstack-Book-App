398446faa80562987087adc9a7e3115bf65b7ae4
Ndimofor <ndimaret@gmail.com>
2023-03-01 13:38:45 +0100

    add css in index

--- src/components/BookCreate.jsx
+++ src/components/BookCreate.jsx
@@ -1,5 +1,3 @@
-import React from "react";
-
 function BookCreate() {
   return (
     <div>
---
+++ src/components/BookList.jsx
@@ -0,0 +1,9 @@
+function BookList() {
+  return (
+    <div>
+      <h1>BookList</h1>
+    </div>
+  );
+}
+
+export default BookList;
