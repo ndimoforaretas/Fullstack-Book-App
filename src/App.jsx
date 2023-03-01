fd633cf6647698e7fcf4e53a75d1913e07e32059
Ndimofor <ndimaret@gmail.com>
2023-03-01 21:26:43 +0100

    add book edit functionality

--- src/App.jsx
+++ src/App.jsx
@@ -6,6 +6,15 @@ import BookList from "./components/BookList";
 function App() {
   const [books, setBooks] = useState([]);
 
+  const editBookById = (id, newTitle) => {
+    const updatedBooks = books.map((book) => {
+      if (book.id === id) {
+        return { ...book, title: newTitle };
+      }
+      return book;
+    });
+    setBooks(updatedBooks);
+  };
   const deleteBookById = (id) => {
     const updatedBooks = books.filter((book) => {
       return book.id !== id;
@@ -27,7 +36,7 @@ function App() {
   return (
     <div className="app">
       <h1>Helloo</h1>
-      <BookList books={books} onDelete={deleteBookById} />
+      <BookList books={books} onDelete={deleteBookById} onEdit={editBookById} />
       <BookCreate onCreate={createBook} />
     </div>
   );
--- src/components/BookEdit.jsx
+++ src/components/BookEdit.jsx
@@ -1,8 +1,22 @@
-function BookEdit() {
+import { useState } from "react";
+
+function BookEdit({ book, onEdit }) {
+  const [title, setTitle] = useState(book.title);
+
+  const handleChange = (event) => {
+    setTitle(event.target.value);
+  };
+  const handleSubmit = (event) => {
+    event.preventDefault();
+
+    onEdit(book.id, title);
+  };
   return (
-    <div>
-      <h1>BookEdit</h1>
-    </div>
+    <form className="book-edit" onSubmit={handleSubmit}>
+      <label>Title</label>
+      <input className="input" value={title} onChange={handleChange} />
+      <button className="button is-primary">Save</button>
+    </form>
   );
 }
 
