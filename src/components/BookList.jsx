6dd5db617751c78066e3739fa8dc1ae274e35acf
Ndimofor <ndimaret@gmail.com>
2023-03-02 13:11:38 +0100

    add conditional rendering

--- src/components/BookList.jsx
+++ src/components/BookList.jsx
@@ -5,10 +5,18 @@ function BookList() {
   //--host 127.0.0.1
 
   const { books } = store();
+
   const renderedBooks = books.map((book) => {
     return <BookShow key={book.id} book={book} />;
   });
-  return <div className="book-list">{renderedBooks}</div>;
+
+  const noBooks = <h2>Your Booklist is empty!</h2>;
+
+  return (
+    <div className="book-list">
+      {books.length !== 0 ? renderedBooks : noBooks}
+    </div>
+  );
 }
 
 export default BookList;
