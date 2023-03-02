681311f0f0656c9e893aa3d49d6a645a307384ac
Ndimofor <ndimaret@gmail.com>
2023-03-02 08:06:03 +0100

    add json-server and rest client extension

--- src/components/BookList.jsx
+++ src/components/BookList.jsx
@@ -1,6 +1,7 @@
 import BookShow from "./BookShow";
 
 function BookList({ books, onDelete, onEdit }) {
+  //--host 127.0.0.1
   const renderedBooks = books.map((book) => {
     return (
       <BookShow onDelete={onDelete} onEdit={onEdit} key={book.id} book={book} />
